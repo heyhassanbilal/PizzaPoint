a37edfa1728b9585b9326d7d09b73adb121ad59e
heyhassanbilal <hassanbilal1837@gmail.com>
2025-02-26 17:56:25 +0530

    Added ViewCartOverlay

---
+++ pizzaPoint/src/components/ViewCartOverlay.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function ViewCartOverlay() {
+return (
+    <>
+        <div className='fixed bottom-0 p-5 left-1/2 transform -translate-x-1/2 flex justify-around items-center bg-brandRed w-[25rem] h-16 rounded-t-lg'>
+                <div className='w-8 h-8 rounded-full border-white border-2'>
+                    <h3 className='text-white  h-full w-full text-center text-lg font-bold'>1</h3>
+                </div>
+                <div className='flex-1'>
+                    <h1 className='text-white font-bold text-center'>View Cart</h1>
+                </div>
+                <div className='w-28 '>
+                    <h3 className='text-white text-right font-bold'>Rs. 0.00</h3>
+                </div>
+        </div>
+    </>
+)
+}
+
+export default ViewCartOverlay
