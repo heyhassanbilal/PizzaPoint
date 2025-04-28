4e173b0be68dd24b66e5b79e858c36b0fe10a3d6
heyhassanbilal <hassanbilal1837@gmail.com>
2025-04-28 14:37:20 +0200

    fixed some bugs

--- pizzaPoint/src/components/BasicCategory.jsx
+++ pizzaPoint/src/components/BasicCategory.jsx
@@ -1,12 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import ServeBanner from '../assets/imgs/ServeDealsBanner.webp'
 import CardLong from './CardLong'
-import serve1 from '../assets/imgs/Serve1.webp'
-import serve2 from '../assets/imgs/Serve2.webp'
-import serve3 from '../assets/imgs/Serve3.webp'
-import serve4 from '../assets/imgs/Serve4.webp'
-import serve5 from '../assets/imgs/Serve5.webp'
-import serve6 from '../assets/imgs/Serve6.webp'
 import { useAuth } from '../utils/useAuth';
 import { useContext } from 'react'
 import { productService, userService } from '../utils/services'
