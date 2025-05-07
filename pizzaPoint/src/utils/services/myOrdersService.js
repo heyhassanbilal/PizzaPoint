bd4fd865d636cf24dcd8b97ca596f3d12be72ac3
heyhassanbilal <hassanbilal1837@gmail.com>
2025-05-07 18:10:48 +0200

    OrderDetail 02

--- pizzaPoint/src/utils/services/myOrdersService.js
+++ pizzaPoint/src/utils/services/myOrdersService.js
@@ -3,4 +3,5 @@ import { apiCore } from './apiCore';
 
 export const myOrdersService = {
   getMyOrders: (email) => apiCore.get(`/api/orders/history/all-orders/${email}`),
+  getOrderById: (id) => apiCore.get(`/api/menuItem/get/${id}`),
 };
