5043240b7dce6239a891f0aeac1d75ff05c7862f
heyhassanbilal <hassanbilal1837@gmail.com>
2025-05-07 08:47:51 +0200

    added orderDetails

--- pizzaPoint/src/App.jsx
+++ pizzaPoint/src/App.jsx
@@ -1,37 +1,37 @@
-import './App.css'
-import { useEffect } from 'react'
-import BackgroundDeals from './components/BackgroundDeals'
+import "./App.css";
+import { useEffect } from "react";
+import BackgroundDeals from "./components/BackgroundDeals";
 // import ProtectedRoute from './components/ProtectedRoute'
-import CategoriesList from './components/CategoriesList'
-import Container from './components/Container'
-import Navbar from './components/Navbar'
+import CategoriesList from "./components/CategoriesList";
+import Container from "./components/Container";
+import Navbar from "./components/Navbar";
 // import SearchBar from './components/SearchBar'
-import ViewCartOverlay from './components/ViewCartOverlay'
-import WhatsAppButton from './components/WhatsAppButton'
-import { useState } from 'react'
-import YourCart from './components/YourCart'
-import SignUp from './components/SignUp'
-import serve6 from './assets/imgs/serve6.webp'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import "./firebase"
+import ViewCartOverlay from "./components/ViewCartOverlay";
+import WhatsAppButton from "./components/WhatsAppButton";
+import { useState } from "react";
+import YourCart from "./components/YourCart";
+import SignUp from "./components/SignUp";
+import serve6 from "./assets/imgs/serve6.webp";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./firebase";
 // import { AuthContext } from './utils/AuthContext'
 // import AuthContext from './utils/AuthContext'
 import { AuthProvider } from "./utils/AuthContext";
-import { CartProvider } from './utils/CartContext';
-import LoginPage from './components/LoginPage'
-import SideMenu from './components/SideMenu'
-import AdminPage from './components/AdminPage'
-import AdminDashboard from './components/AdminDashboard'
-import AddressChange from './components/AddressChange'
-import CheckOut from './components/CheckOut'
+import { CartProvider } from "./utils/CartContext";
+import LoginPage from "./components/LoginPage";
+import SideMenu from "./components/SideMenu";
+import AdminPage from "./components/AdminPage";
+import AdminDashboard from "./components/AdminDashboard";
+import AddressChange from "./components/AddressChange";
+import CheckOut from "./components/CheckOut";
 // import { useAuth } from './utils/useAuth'
-import MyOrders from './components/MyOrders'
+import MyOrders from "./components/MyOrders";
+import OrderDetails from "./components/OrderDetails";
 
 function App() {
-
   // In your parent component
   const [isCartOpen, setIsCartOpen] = useState(false);
-  
+
   // To open cart
   const openCart = () => setIsCartOpen(true);
 
@@ -47,18 +47,28 @@ function App() {
   const closeSideMenu = () => setSideMenuOpen(false);
 
   const [cartItems, setCartItems] = useState([
-    { id: 1, name: "Super Max (Small 7 Inch)", price: 649, qty: 1, img: {serve6} },
-    { id: 2, name: "Winter Deal 01", price: 399, qty: 5, img: {serve6}, extra: "Fajita Sensation Pizza" }
+    {
+      id: 1,
+      name: "Super Max (Small 7 Inch)",
+      price: 649,
+      qty: 1,
+      img: { serve6 },
+    },
+    {
+      id: 2,
+      name: "Winter Deal 01",
+      price: 399,
+      qty: 5,
+      img: { serve6 },
+      extra: "Fajita Sensation Pizza",
+    },
   ]);
 
   const popularItems = [
-    { id: 3, name: "Plain Garlic Bread", price: 249, img: {serve6} },
-    { id: 4, name: "7up", price: 99, img: {serve6} },
-    { id: 5, name: "Pepsi", price: 99, img: {serve6} }
+    { id: 3, name: "Plain Garlic Bread", price: 249, img: { serve6 } },
+    { id: 4, name: "7up", price: 99, img: { serve6 } },
+    { id: 5, name: "Pepsi", price: 99, img: { serve6 } },
   ];
-
-  
-
 
   // useEffect(() => {
   //   if (token) {
@@ -71,83 +81,117 @@ function App() {
       <CartProvider>
         <Router>
           <Routes>
-            <Route path="/" element={
-              
-              <div className='h-screen overflow-y-auto overflow-x-hidden'>
-                
-                {isCartOpen &&  <YourCart cartItems={cartItems} 
-                            popularItems={popularItems} 
-                            setCartItems={setCartItems} 
-                            isOpen={isCartOpen}
-                            onClose={closeCart}
-                            />}
+            <Route
+              path="/"
+              element={
+                <div className="h-screen overflow-y-auto overflow-x-hidden">
+                  {isCartOpen && (
+                    <YourCart
+                      cartItems={cartItems}
+                      popularItems={popularItems}
+                      setCartItems={setCartItems}
+                      isOpen={isCartOpen}
+                      onClose={closeCart}
+                    />
+                  )}
 
-                {isSideMenuOpen && <SideMenu isSideMenuOpen={isSideMenuOpen} onSideMenuClose={closeSideMenu} />}
-                  <div className='flex-col'>
-                    <Navbar setIsCartOpen= {setIsCartOpen} setSideMenuOpen = {setSideMenuOpen} />
-                    <BackgroundDeals/>
-                    <WhatsAppButton />        
+                  {isSideMenuOpen && (
+                    <SideMenu
+                      isSideMenuOpen={isSideMenuOpen}
+                      onSideMenuClose={closeSideMenu}
+                    />
+                  )}
+                  <div className="flex-col">
+                    <Navbar
+                      setIsCartOpen={setIsCartOpen}
+                      setSideMenuOpen={setSideMenuOpen}
+                    />
+                    <BackgroundDeals />
+                    <WhatsAppButton />
                   </div>
-                    <CategoriesList />
-                  <div className='flex justify-center'>
-                    <Container/>
+                  <CategoriesList />
+                  <div className="flex justify-center">
+                    <Container />
                   </div>
-                    <ViewCartOverlay setIsCartOpen= {setIsCartOpen} />
-                  
-
+                  <ViewCartOverlay setIsCartOpen={setIsCartOpen} />
                 </div>
-            } /> 
-          
-            <Route path="/signup" element={
-              <>
-                <Navbar setIsCartOpen= {setIsCartOpen} setSideMenuOpen = {setSideMenuOpen}/>
-                <SignUp />
-              </>  
-          } />
-            <Route path="/login" element={
-              <>
-                <Navbar setIsCartOpen= {setIsCartOpen} setSideMenuOpen = {setSideMenuOpen}/>
-                <LoginPage />
-              </>  
-          } />
-
-            <Route path="/admin" element={
-              <>
-                <AdminPage />
-              </>  
-            } />
-
-            <Route path="/adminP" element={
-              <>
-                <AdminDashboard />
-              </>  
-            } />
-
-            <Route path="/address" element={
-              <>
-                <AddressChange  />
-              </>  
-            } />
-
-            <Route path="/checkout" element={
-              <>
-                <CheckOut />
-              </>  
-            } />
-            <Route path="/orders" element={
-              <>
-                <MyOrders />
-              </>  
-            } />
-            <Route path="/orders/:orderId" element=
-              {<OrderDetails />} 
+              }
             />
 
+            <Route
+              path="/signup"
+              element={
+                <>
+                  <Navbar
+                    setIsCartOpen={setIsCartOpen}
+                    setSideMenuOpen={setSideMenuOpen}
+                  />
+                  <SignUp />
+                </>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <>
+                  <Navbar
+                    setIsCartOpen={setIsCartOpen}
+                    setSideMenuOpen={setSideMenuOpen}
+                  />
+                  <LoginPage />
+                </>
+              }
+            />
+
+            <Route
+              path="/admin"
+              element={
+                <>
+                  <AdminPage />
+                </>
+              }
+            />
+
+            <Route
+              path="/adminP"
+              element={
+                <>
+                  <AdminDashboard />
+                </>
+              }
+            />
+
+            <Route
+              path="/address"
+              element={
+                <>
+                  <AddressChange />
+                </>
+              }
+            />
+
+            <Route
+              path="/checkout"
+              element={
+                <>
+                  <CheckOut />
+                </>
+              }
+            />
+            <Route
+              path="/orders"
+              element={
+                <>
+                  <MyOrders />
+                </>
+              }
+            />
+            <Route path="/orders/:orderId" element={<OrderDetails />} />
           </Routes>
         </Router>
       </CartProvider>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
