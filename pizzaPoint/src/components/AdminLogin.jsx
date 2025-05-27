5e66b77b4203f5e9895515b40695c7200539188c
heyhassanbilal <hassanbilal1837@gmail.com>
2025-05-27 18:11:30 +0200

    admin login 01

--- pizzaPoint/src/components/AdminLogin.jsx
+++ pizzaPoint/src/components/AdminLogin.jsx
@@ -2,6 +2,7 @@ import { React, useState, useContext, useEffect } from "react";
 import { Route, useNavigate } from "react-router-dom";
 import { useAuth } from "../utils/useAuth";
 import { authService } from "../utils/services";
+import { use } from "react";
 
 export default function AdminLogin() {
   const { setToken, setEmail } = useAuth();
@@ -23,7 +24,7 @@ export default function AdminLogin() {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("Form Data Submitted:", formData);
 
@@ -37,46 +38,48 @@ export default function AdminLogin() {
       //   body: JSON.stringify(formData),
       // })
       //   .then((response) => response.json())
-        // .then((data) => {
-          const data = await authService.adminLogin(formData);
-          setStatus(data.status);
-          if (!data.status) {
-            setError("Invalid email or password");
-            return;
-          }
-          setFormStep("otp");
-          console.log("Success:", data);
-        //   setToken(data.token);
-        //   setEmail(data.email);
-        //   navigate("/adminP");
-          // alert("User login successfull");
-        // });
+      // .then((data) => {
+      const data = await authService.adminLogin(formData);
+      setStatus(data.otpSent);
+      if (status) {
+        setError("Invalid email or password");
+        return;
+      }
+      setFormStep("otp");
+      console.log("Success:", data);
+      //   setToken(data.token);
+      //   setEmail(data.email);
+      //   navigate("/adminP");
+      // alert("User login successfull");
+      // });
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
-    const handleOTPSubmit = async (e) => {
-        e.preventDefault();
-        try {
-        const otp = verificationCode.trim();
-        if (!otp) {
-            setError("OTP is required");
-            return;
-        }
-        const data = await authService.adminOTPcheck({ email: formData.email, otp });
-        if (data.status) {
-            setToken(data.token);
-            setEmail(data.email);
-            navigate("/adminP");
-        } else {
-            setError("Invalid OTP");
-        }
-        } catch (error) {
-        console.error("Error:", error);
-        }
+  const handleOTPSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const otp = verificationCode.trim();
+      if (!otp) {
+        setError("OTP is required");
+        return;
+      }
+      const data = await authService.adminOTPcheck({
+        email: formData.email,
+        otp,
+      });
+      if (data.status) {
+        setToken(data.token);
+        setEmail(data.email);
+        navigate("/adminP");
+      } else {
+        setError("Invalid OTP");
+      }
+    } catch (error) {
+      console.error("Error:", error);
     }
-  
+  };
 
   return (
     <div className="flex items-center justify-center min-h-[93.5vh] bg-[#dc2626]">
@@ -84,38 +87,41 @@ export default function AdminLogin() {
         <h2 className="text-2xl font-bold text-center mb-4">Login</h2>
 
         {formStep === "form" ? (
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-sm font-semibold mb-1">Email</label>
-            <input
-              type="email"
-              name="email"
-              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-500"
-              placeholder="Enter your email"
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-semibold mb-1">Password</label>
-            <input
-              type="password"
-              name="password"
-              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-500"
-              placeholder="Enter your password"
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition"
-          >
-            Login
-          </button>
-          {error && <p style={{ color: "red" }}>{error}</p>}
-        </form>) : (
-            <form onSubmit={handleOTPSubmit}>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label className="block text-sm font-semibold mb-1">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-500"
+                placeholder="Enter your email"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-semibold mb-1">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-500"
+                placeholder="Enter your password"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition"
+            >
+              Login
+            </button>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+          </form>
+        ) : (
+          <form onSubmit={handleOTPSubmit}>
             <input
               type="text"
               name="otp"
