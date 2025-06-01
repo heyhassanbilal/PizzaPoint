2b1ac446759ea7e8c19d9516e3df96ea35dc4527
heyhassanbilal <hassanbilal1837@gmail.com>
2025-06-01 22:08:57 +0200

    mobile view 2 cards 03

--- pizzaPoint/src/components/CardLong.jsx
+++ pizzaPoint/src/components/CardLong.jsx
@@ -11,18 +11,18 @@ function CardLong({img,title,price,description,size, id}) {
     return (
         <>
         {cardOpen && <ReviewCard img={img} InitialId={id} title={title} Initialprice={price} description={description} setCardOpen={setCardOpen} size={size}/>}
-        <div className='h-[14rem] w-32 md:h-[28rem] md:w-64 rounded-xl shadow-2xl border-4 border-transparent transition-all duration-300 hover:scale-105 hover:border-brandRed hover:cursor-pointer' onClick={()=> setCardOpen(true)}>
-        <div className='h-32 md:h-64 relative'>
-            <img src={img} alt="" className='h-full w-full rounded-t-xl shadow-md' /> 
-        </div>
-            <div className='flex-col pt-2'>
-                <h1 className='text-center font-bold truncate'>{title}</h1>
-                <p className='text-center text-sm mt-5 truncate'>{description}</p>
-                <h2 className='text-center text-sm font-bold mt-9'>{price} HUF</h2>
-                <div className='text-center mt-2'>
-                    <button className='text-center text-white text-sm bg-brandRed pt-1 pb-1 pr-10 pl-10 rounded-xl transition-transform duration-300 hover:scale-105'>Add to Cart</button>
-                </div>
+        <div className='h-[28rem] w-64 flex-shrink rounded-xl shadow-2xl border-4 border-transparent transition-all duration-300 hover:scale-105 hover:border-brandRed hover:cursor-pointer' onClick={()=> setCardOpen(true)}>
+            <div className='h-64 relative'>
+                <img src={img} alt="" className='h-full w-full rounded-t-xl shadow-md' /> 
             </div>
+                <div className='flex-col pt-2'>
+                    <h1 className='text-center font-bold truncate'>{title}</h1>
+                    <p className='text-center text-sm mt-5 truncate'>{description}</p>
+                    <h2 className='text-center text-sm font-bold mt-9'>{price} HUF</h2>
+                    <div className='text-center mt-2'>
+                        <button className='text-center text-white text-sm bg-brandRed pt-1 pb-1 pr-10 pl-10 rounded-xl transition-transform duration-300 hover:scale-105'>Add to Cart</button>
+                    </div>
+                </div>
         </div>
     </>
   )
