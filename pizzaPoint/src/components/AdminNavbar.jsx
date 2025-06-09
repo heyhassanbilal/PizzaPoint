727d93e46fedafcaf8f1d8867883a649b7bfd757
heyhassanbilal <hassanbilal1837@gmail.com>
2025-06-09 20:28:18 +0200

    fixed adminpage 1

--- pizzaPoint/src/components/AdminNavbar.jsx
+++ pizzaPoint/src/components/AdminNavbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState,useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useCart } from "../utils/CartContext";
 
