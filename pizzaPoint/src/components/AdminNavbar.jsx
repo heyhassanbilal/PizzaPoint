4c3e67fbff690a1c750b62ebe043621094a38963
heyhassanbilal <hassanbilal1837@gmail.com>
2025-06-23 20:22:45 +0200

    open resturant

--- pizzaPoint/src/components/AdminNavbar.jsx
+++ pizzaPoint/src/components/AdminNavbar.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { useCart } from "../utils/CartContext";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../utils/useAuth";
+import { authService } from "../utils/services";
 
 function AdminNavbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -54,6 +55,15 @@ function AdminNavbar() {
     };
   }, []);
 
+  const handleSetIsOpen = async () => {
+    try {
+      setIsOpen((p) => !p);
+      const response = await authService.openRestaurant(isOpen);
+    } catch (error) {
+      console.error("Error updating restaurant status:", error);
+    }
+  }
+
   return (
     <>
       <nav className="fixed top-0 w-full h-16 flex bg-white border-b-2 ">
@@ -66,7 +76,7 @@ function AdminNavbar() {
             className={`${
               isOpen ? "bg-black" : "bg-gray-600"
             } mt-1 w-32 mr-2 h-11 rounded-2xl text-center hover:cursor-pointer hover:scale-105`}
-            onClick={() => setIsOpen((p) => !p)}
+            onClick={() => handleSetIsOpen()}
           >
             <p className="text-2xl text-white pt-1">
               {isOpen ? "Opened" : "Closed"}
@@ -113,7 +123,7 @@ function AdminNavbar() {
 
           <button
             className="w-10 h-10 mr-5 mt-1 pl-3 bg-black rounded-lg flex-col  hover:cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out"
-            onClick={() => setSideMenuOpen(true)}
+            onClick={() => setIsMenuOpen(true)}
           >
             <div className="w-3/5 h-[3px] bg-white mb-1 rounded-sm"></div>
             <div className="w-3/5 h-[3px] bg-white mb-1 rounded-sm"></div>
