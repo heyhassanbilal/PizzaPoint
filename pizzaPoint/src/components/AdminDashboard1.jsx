a9f3799967cf30107bf26432cb62e9508f6b05e0
heyhassanbilal <hassanbilal1837@gmail.com>
2025-06-25 21:04:24 +0200

    testing admin page 14

--- pizzaPoint/src/components/AdminDashboard1.jsx
+++ pizzaPoint/src/components/AdminDashboard1.jsx
@@ -111,6 +111,14 @@ function AdminDashboard1() {
     );
   };
 
+  const options = {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+
   return (
     <div className="w-full">
       <AdminNavbar propIsOpen={isOpen} />
